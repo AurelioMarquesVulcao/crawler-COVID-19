719a850ce1e1a70af60fe8b90e9ef3492ff667f4
Aurelio Vulcão <vulcao@vulcaotech.com>
2020-03-13 17:40:00 -0300

    testing time interval I can repeat the task of searching

--- app/covid19.js
+++ app/covid19.js
@@ -4,6 +4,9 @@ const axios = require('axios')
 const notices = "https://www.worldometers.info/coronavirus/#countries"
 var countr = {}
 
+
+
+setInterval(function() {
 const LeanResponse = (html) => {
     let $ = cheerio.load(html)
     var covid =  $('tbody tr').map((index, element) => ({
@@ -14,7 +17,7 @@ const LeanResponse = (html) => {
         // countr:  $(element).find('td').text().replace(',', ".").split(' ')[2] = String ? $(element).find('td').text().split(' ')[1]+$(element).find('td').text().split(' ')[2] : $(element).find('td').text().split(' ')[1]
         countr:  $(element).find("td").slice(0,1).text(),
         cases:  $(element).find("td").slice(1,2).text(),
-        deth:  $(element).find("td").slice(3,4).text()
+        death:  $(element).find("td").slice(3,4).text()
     })).get()
     return covid
 }
@@ -30,6 +33,20 @@ const SearchNotices = async (LeanResponse) =>{
     }
 }
 
+
+
 SearchNotices(LeanResponse)
     .then(resp => console.log('response', resp))
     .catch(err => console.log('erro', err))
+
+
+function RetornaDataHoraAtual(){
+    var dNow = new Date();
+    var localdate = dNow.getDate() + '/' + (dNow.getMonth()+1) + '/' + dNow.getFullYear() + ' ' + dNow.getHours() + ':' + dNow.getMinutes();
+    return localdate;
+  }
+
+  console.log(RetornaDataHoraAtual())
+
+}, 120000000);
+
