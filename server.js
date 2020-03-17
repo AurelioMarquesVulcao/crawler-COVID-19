45bf2d346ce404a393e702d180858dfa6021cc1c
Aurelio Vulcão <vulcao@vulcaotech.com>
2020-03-17 15:36:18 -0300

    Docker ok. Corrected application. commenting the code.

--- server.js
+++ server.js
@@ -1,27 +1,9 @@
-// lingando servidor
-// npm start
-
 const http = require("http");
 const app = require("./src/app");
 
 const port = process.env.Port || 3000;
+
 const server = http.createServer(app);
 server.listen(port);
+console.log('server ok')
 
-// const express = require("express");
-// const conectDB = require("./db/conection");
-
-// const app = express();
-
-// conectDB();
-
-// app.use(express.json({ extended: false }));
-
-// app.use("/api/userModel", require("./api/User"));
-
-// const Port = process.env.Port || 3000;
-
-// app.listen(Port, () => console.log("Server Started"));
-
-// lingando servidor
-// npm start
